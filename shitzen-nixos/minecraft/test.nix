f9e84b8e6a0468a34065961bff19f97526391279
Vali0004 <78507032+Vali0004@users.noreply.github.com>
2025-01-26 22:29:10 -0500

    Change modpacks again

--- shitzen-nixos/minecraft/test.nix
+++ shitzen-nixos/minecraft/test.nix
@@ -1,12 +1,24 @@
 {
-  "server-icon.png" = {
-    hash = "sha256-9QvXb9oxBpEIJGV0S25ofyriKTK5PUIt6b1z9uEvRW4=";
-    url = "https://fuckk.lol/minecraft/image.png";
+  "config" = {
+    src = ./modpack/config;
+  };
+  "defaultconfigs" = {
+    src = ./modpack/defaultconfigs;
+  };
+  "kubejs" = {
+    src = ./modpack/kubejs;
   };
   "mods" = {
     src = ./modpack/mods;
   };
-  "config" = {
-    src = ./modpack/config;
+  "resources" = {
+    src = ./modpack/resources;
+  };
+  "scripts" = {
+    src = ./modpack/scripts;
+  };
+  "server-icon.png" = {
+    hash = "sha256-9QvXb9oxBpEIJGV0S25ofyriKTK5PUIt6b1z9uEvRW4=";
+    url = "https://fuckk.lol/minecraft/image.png";
   };
 }
