591daa68101235d5412cb21ef7e74b20fced35a8
Vali0004 <78507032+Vali0004@users.noreply.github.com>
2025-02-21 00:36:51 -0500

    Get VR to play nice on nixos-amd

--- nixos-amd/configuration.nix
+++ nixos-amd/configuration.nix
@@ -13,15 +13,27 @@ in {
     spice.nixosModules.default
   ];
 
-  boot.loader = {
-    grub = {
-      enable = true;
-      copyKernels = true;
-      device = "nodev";
-      efiSupport = true;
-      efiInstallAsRemovable = false;
+  boot = {
+    loader = {
+      grub = {
+        enable = true;
+        copyKernels = true;
+        device = "nodev";
+        efiSupport = true;
+        efiInstallAsRemovable = false;
+      };
+      efi.canTouchEfiVariables = true;
     };
-    efi.canTouchEfiVariables = true;
+    kernelPatches = [
+      {
+        name = "amdgpu-ignore-ctx-privileges";
+        patch = pkgs.fetchpatch {
+          name = "cap_sys_nice_begone.patch";
+          url = "https://github.com/Frogging-Family/community-patches/raw/master/linux61-tkg/cap_sys_nice_begone.mypatch";
+          hash = "sha256-Y3a0+x2xvHsfLax/uwycdJf3xLxvVfkfDVqjkxNaYEo=";
+        };
+      }
+    ];
   };
 
   console = {
@@ -37,6 +49,7 @@ in {
     systemPackages = with pkgs; [
       alacritty
       alsa-utils
+      alvr
       curl
       dos2unix
       envsubst
@@ -48,13 +61,13 @@ in {
       iperf
       mpv
       neovim
+      obs-studio
       openssl
       pavucontrol
       pciutils
       picom
       playerctl
       pulseaudio
-      spotify
       spicetify-cli
       steamcmd
       sysstat
@@ -93,7 +106,15 @@ in {
   nixpkgs.config.allowUnfree = true;
 
   programs = {
+    alvr = {
+      enable = true;
+      openFirewall = true;
+    };
     command-not-found.enable = true;
+    git = {
+      enable = true;
+      lfs.enable = true;
+    };
     java.enable = true;
     spicetify = {
       enable = true;
@@ -153,6 +174,35 @@ in {
       xkb = {
         layout = "us";
       };
+    };
+  };
+
+  services = {
+    wivrn = {
+      enable = true;
+      autoStart = true;
+      defaultRuntime = true;
+      config = {
+        enable = true;
+        json = {
+          # 1.0x foveation scaling
+          scale = 1.0;
+          # 100 Mb/s
+          bitrate = 100000000;
+          encoders = [
+            {
+              encoder = "vaapi";
+              codec = "h265";
+              # 1.0 x 1.0 scaling
+              width = 1.0;
+              height = 1.0;
+              offset_x = 0.0;
+              offset_y = 0.0;
+            }
+          ];
+        };
+      };
+      openFirewall = true;
     };
   };
 
