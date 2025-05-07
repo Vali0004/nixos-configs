afab72fa9ac5054598a27486089b9c1888b3b94e
Vali0004 <78507032+Vali0004@users.noreply.github.com>
2025-05-07 04:52:56 -0400

    Ensure all files have newlines

--- nixos-amd/home-manager/services/dunst.nix
+++ nixos-amd/home-manager/services/dunst.nix
@@ -1,21 +1,19 @@
 { config, lib, ... }:
 
 {
-  services = {
-    dunst = {
-      enable = true;
-      settings = {
-        global = {
-          font = "DejaVu Sans Mono 11";
-          origin = "top-right";
-          scale = 1;
-          transparency = 20;
-        };
-        urgency_normal = {
-          background = "#37474f";
-          foreground = "#eceff1";
-          timeout = 10;
-        };
+  services.dunst = {
+    enable = true;
+    settings = {
+      global = {
+        font = "DejaVu Sans Mono 11";
+        origin = "top-right";
+        scale = 1;
+        transparency = 20;
+      };
+      urgency_normal = {
+        background = "#37474f";
+        foreground = "#eceff1";
+        timeout = 10;
       };
     };
   };
