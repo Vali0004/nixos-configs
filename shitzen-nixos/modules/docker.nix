ab1adb23ad204696c19cdf60f4d0c0be4bb2da03
Vali0004 <78507032+Vali0004@users.noreply.github.com>
2025-05-31 20:41:54 -0400

    Attempt to use dockge

--- shitzen-nixos/modules/docker.nix
+++ shitzen-nixos/modules/docker.nix
@@ -2,12 +2,25 @@
 
 {
   virtualisation = {
-    #podman.enable = true;
-    #oci-containers.containers = {
-    #  dockge = {
-    #    image = "$GITHUB_BRANCH$:louislam/dockge:master$";
-    #    entrypoint = "/data/dockge";
-    #  };
-    #};
+    podman.enable = true;
+    oci-containers.containers = {
+      dockge = {
+        autoStart = true;
+        image = "louislam/dockge:1";
+        ports = [
+          "5001:5001"
+        ];
+        volumes = [
+          "/var/run/docker.sock:/var/run/docker.sock"
+          "./data:/app/data"
+          "/data/dockge/stacks:/data/dockge/stacks"
+        ];
+        entrypoint = "/data/dockge/dockge-run";
+        environment = {
+          DOCKGE_STACKS_DIR = "/data/dockge/stacks";
+        };
+        extraOptions = [ "--cap-add=NET_ADMIN" ];
+      };
+    };
   };
 }
--- shitzen-nixos/services/wings.nix
+++ shitzen-nixos/services/wings.nix
@@ -41,8 +41,4 @@
       };
     };
   };
-
-  virtualisation = {
-    docker.enable = true;
-  };
 }
