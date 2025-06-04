dc0e6c6809e61beb62d0f1cc89e070c49f36132a
Vali0004 <78507032+Vali0004@users.noreply.github.com>
2025-06-03 22:54:26 -0400

    Add domain for Jellyfin

--- shitzen-nixos/services/nginx/furryporn_ca.nix
+++ shitzen-nixos/services/nginx/furryporn_ca.nix
@@ -12,6 +12,24 @@
         };
       };
     };
+    # Jellyfin
+    "watch.furryporn.ca" = {
+      enableACME = true;
+      forceSSL = true;
+      locations = {
+        "/" = mkProxy {
+          port = 8096;
+          webSockets = true;
+          config = ''
+            proxy_ssl_server_name on;
+            proxy_ssl_name $proxy_host;
+            proxy_cache_bypass $http_upgrade;
+            proxy_set_header X-Requested-With XMLHttpRequest;
+          '';
+        };
+      };
+    };
+    # Unison ptero
     "unison.fuckk.lol" = {
       enableACME = true;
       forceSSL = true;
