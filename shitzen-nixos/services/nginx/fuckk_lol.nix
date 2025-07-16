90368ec4b7e169958c5c0187f93806a41e2d4d58
Vali0004 <diorcheats.vali@gmail.com>
2025-07-15 23:27:08 -0400

    Cleanup nginx config

--- shitzen-nixos/services/nginx/fuckk_lol.nix
+++ shitzen-nixos/services/nginx/fuckk_lol.nix
@@ -8,69 +8,64 @@ in {
     "smtp.fuckk.lol" = {
       enableACME = true;
       forceSSL = true;
-      locations = {
-        "/" = {
-          extraConfig = ''
-            return 404;
-          '';
-        };
-      };
+      locations."/".extraConfig = ''
+        return 404;
+      '';
     };
     # Unison
     "unison.fuckk.lol" = {
       enableACME = true;
       forceSSL = true;
-      locations = {
-        "/" = {
-          extraConfig = ''
-            return 404;
-          '';
-        };
-      };
+      locations."/".extraConfig = ''
+        return 404;
+      '';
     };
     # Dockge
     "dockge.fuckk.lol" = {
       enableACME = true;
       forceSSL = true;
-      locations = {
-        "/" = mkProxy {
-          port = 5001;
-          webSockets = true;
-          config = ''
-            proxy_ssl_server_name on;
-            proxy_ssl_name $proxy_host;
-            proxy_cache_bypass $http_upgrade;
-            proxy_set_header X-Requested-With XMLHttpRequest;
-          '';
-        };
+      locations."/" = mkProxy {
+        port = 5001;
+        webSockets = true;
+        config = ''
+          proxy_ssl_server_name on;
+          proxy_ssl_name $proxy_host;
+          proxy_cache_bypass $http_upgrade;
+          proxy_set_header X-Requested-With XMLHttpRequest;
+        '';
       };
     };
     # Sonarr
     "sonarr.fuckk.lol" = {
       enableACME = true;
       forceSSL = true;
-      locations = {
-        "/" = {
-          proxyPass = "http://127.0.0.1:8989";
-          proxyWebsockets = true;
-        };
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:8989";
+        proxyWebsockets = true;
+      };
+    };
+    # Proxmox
+    "proxmox.fuckk.lol" = {
+      enableACME = true;
+      forceSSL = true;
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:8006";
+        proxyWebsockets = true;
       };
     };
     # Cors anywhere
     "r34.fuckk.lol" = {
       enableACME = true;
       forceSSL = true;
-      locations = {
-        "/" = mkProxy {
-          port = 8099;
-          webSockets = true;
-          config = ''
-            proxy_ssl_server_name on;
-            proxy_ssl_name $proxy_host;
-            proxy_cache_bypass $http_upgrade;
-            proxy_set_header X-Requested-With XMLHttpRequest;
-          '';
-        };
+      locations."/" = mkProxy {
+        port = 8099;
+        webSockets = true;
+        config = ''
+          proxy_ssl_server_name on;
+          proxy_ssl_name $proxy_host;
+          proxy_cache_bypass $http_upgrade;
+          proxy_set_header X-Requested-With XMLHttpRequest;
+        '';
       };
     };
   };
--- shitzen-nixos/services/nginx/furryporn_ca.nix
+++ shitzen-nixos/services/nginx/furryporn_ca.nix
@@ -7,22 +7,18 @@ in {
     "valis.furryporn.ca" = {
       enableACME = true;
       forceSSL = true;
-      locations = {
-        "/" = {
-          alias = "/data/valisfurryporn/";
-          index = "index.html";
-        };
+      locations."/" = {
+        alias = "/data/valisfurryporn/";
+        index = "index.html";
       };
     };
     # Jellyfin
     "watch.furryporn.ca" = {
       enableACME = true;
       forceSSL = true;
-      locations = {
-        "/" = {
-          proxyPass = "http://127.0.0.1:8096";
-          proxyWebsockets = true;
-        };
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:8096";
+        proxyWebsockets = true;
       };
     };
   };
