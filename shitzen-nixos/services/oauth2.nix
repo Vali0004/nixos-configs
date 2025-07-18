71dde66182d643f2dd755cb67d8494cd9a28775c
Vali0004 <diorcheats.vali@gmail.com>
2025-07-18 04:12:07 -0400

    Fix oauth2 proxy

--- shitzen-nixos/services/oauth2.nix
+++ shitzen-nixos/services/oauth2.nix
@@ -1,25 +1,17 @@
 { config, inputs, lib, pkgs, ... }:
 
 {
-  systemd.services.oauth2-proxy.serviceConfig.EnvironmentFile = "${config.age.secrets.oauth2.path}";
   services.oauth2-proxy = {
     cookie = {
-      secret = "";
       secure = true;
       refresh = "1h";
     };
+    keyFile = "${config.age.secrets.oauth2.path}";
     email.domains = [ "fuckk.lol" ];
     enable = true;
     redirectURL = "https://watch.furryporn.ca/oauth2/callback";
     provider = "google";
     setXauthrequest = true;
-    clientID = "";
-    clientSecret = "";
-    extraConfig = {
-      client_id = "$OAUTH2_PROXY_CLIENT_ID";
-      client_secret = "$OAUTH2_PROXY_CLIENT_SECRET";
-      cookie_secret = "$OAUTH2_PROXY_COOKIE_SECRET";
-    };
   };
 
   services.nginx.virtualHosts = {
