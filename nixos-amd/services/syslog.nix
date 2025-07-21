2ba491032f6ba92d26a8edaff9275b40b5defba8
Vali0004 <diorcheats.vali@gmail.com>
2025-07-21 08:39:39 -0400

    Add element, and irssi. Fix syslog

--- nixos-amd/services/syslog.nix
+++ nixos-amd/services/syslog.nix
@@ -5,7 +5,7 @@
     enable = true;
     extraConfig = ''
       source src {
-        udp(ip(10.0.0.244) port(6666));
+        udp(ip(0.0.0.0) port(6666));
       };
 
       destination shitzenlog { file("/var/log/shitzen-nixos.log"); };
