b520049d3258f72238c5e6db5edeed7358684cd1
Vali0004 <diorcheats.vali@gmail.com>
2025-07-30 15:19:43 -0400

    Setup NFS port openning

--- shitzen-nixos/services/nfs.nix
+++ shitzen-nixos/services/nfs.nix
@@ -1,8 +1,23 @@
 { config, inputs, lib, pkgs, ... }:
 
-{
+let
+  statd-port = 4000;
+  lockd-port = 4001;
+  mountd-port = 4002;
+in {
+  networking.firewall.allowedTCPPorts = [
+    111 # NFS Portmapper
+    2049 # NFS Traffic
+    statd-port
+    lockd-port
+    mountd-port
+  ];
+
   services.nfs.server = {
     enable = true;
+    statdPort = statd-port;
+    lockdPort = lockd-port;
+    mountdPort = mountd-port;
     exports = ''
       /data 10.0.0.201(rw,sync,no_subtree_check,no_root_squash) 10.0.0.202(rw,sync,no_subtree_check,no_root_squash) 10.0.0.190(rw,sync,no_subtree_check,no_root_squash)
     '';
