6bfb44a37daefdfa225025a576da4b468f2c1cd8
Vali0004 <diorcheats.vali@gmail.com>
2025-08-18 02:57:16 -0400

    Make pnp pure

--- shitzen-nixos/modules/pnp-loader/api/package.nix
+++ shitzen-nixos/modules/pnp-loader/api/package.nix
@@ -1,5 +1,6 @@
 { lib
 , buildNpmPackage
+, requireFile
 }:
 
 buildNpmPackage {
@@ -7,5 +8,9 @@ buildNpmPackage {
   pname = "pnp-api";
   version = "0.1.0";
   npmDepsHash = "sha256-u2TQ9TmuyjKLrPdyKXBs+taYOf97Rl63E1fh08jiYWw=";
-  src = /home/vali/pnp-api;
+  src = requireFile {
+    name = "pnp-api.tar.xz";
+    url = "file:///home/vali/pnp-api.tar.xz";
+    sha256 = "sha256-zVoomaUxPXi4NpsPMDsloiWh+QDnlHh2COfb3XXjkow=";
+  };
 }
