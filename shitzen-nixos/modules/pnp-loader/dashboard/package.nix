c9fef35191811fb1f0b0e9b03dde0115fd698be0
Vali0004 <diorcheats.vali@gmail.com>
2025-08-18 02:57:16 -0400

    Make pnp pure

--- shitzen-nixos/modules/pnp-loader/dashboard/package.nix
+++ shitzen-nixos/modules/pnp-loader/dashboard/package.nix
@@ -1,16 +1,21 @@
 { lib
 , buildNpmPackage
+, requireFile
 }:
 
 buildNpmPackage {
   pname = "pnp-dashboard";
   version = "0.1.0";
 
-  src = /home/vali/pnp-panel;
+  npmBuildScript = "build";
 
   npmDepsHash = "sha256-T8sBF2Sr9YX3GZTdHjjV3UQdj7bNM29+xOjA6FB4t7c=";
 
-  npmBuildScript = "build";
+  src = requireFile {
+    name = "pnp-panel.tar.xz";
+    url = "file:///home/vali/pnp-panel.tar.xz";
+    sha256 = "sha256-IWh/x0i1VwFAY7TgEbAYR/Y0Ry9ORkluQHBu2VZEr78=";
+  };
 
   installPhase = ''
     mkdir -p $out
