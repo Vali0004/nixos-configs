3aab4088b7aebfe22a1155095634f099b06d8f50
Vali0004 <diorcheats.vali@gmail.com>
2025-09-19 03:43:10 -0400

    Fix zdb (zfs-fragmentation)

--- lenovo/services/zdb.nix
+++ lenovo/services/zdb.nix
@@ -1,10 +1,13 @@
 { pkgs, ... }:
 
-{
+let
+  zfsUtil = builtins.getFlake "github:cleverca22/zfs-utils";
+  zfsUtilPkgs = zfsUtil.outputs.packages.x86_64-linux;
+in {
   networking.firewall.allowedTCPPorts = [ 9103 ];
 
   systemd.services.zfs-fragmentation = {
-    serviceConfig.ExecStart = "${pkgs.zfs-fragmentation}/bin/zfs-fragmentation";
+    serviceConfig.ExecStart = "${zfsUtilPkgs.zfs-fragmentation}/bin/zfs-fragmentation";
     wantedBy = [ "multi-user.target" ];
     serviceConfig.Restart = "always";
   };
