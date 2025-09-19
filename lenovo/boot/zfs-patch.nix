fc83d045aba37aec872772b15b87dd2bcc27eb2b
Vali0004 <diorcheats.vali@gmail.com>
2025-09-19 02:23:59 -0400

    Update zfs patch hash

--- lenovo/boot/zfs-patch.nix
+++ lenovo/boot/zfs-patch.nix
@@ -3,7 +3,7 @@
 let
   zfs-src = pkgs.fetchurl {
     url = "https://github.com/openzfs/zfs/archive/pull/14013/head.tar.gz";
-    hash = "sha256-tGbk4m5G7jJjf3bSd01BZWq2PFnUwYDMKx17tQ23iSU=";
+    hash = "sha256-X4PO6uf/ppEedR6ZAoWmrDRfHXxv2LuBThekRZOwmoA=";
   };
 in {
   boot.kernelPackages = pkgs.linuxPackages.extend (self: super: {
