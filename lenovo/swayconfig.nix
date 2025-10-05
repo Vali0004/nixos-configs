d8a13d5fc13f42c1c80a9f82b31294814b0499ff
Vali0004 <diorcheats.vali@gmail.com>
2025-10-05 05:29:47 -0400

    Add sway config

---
+++ lenovo/swayconfig.nix
@@ -0,0 +1,11 @@
+{
+  # Modifer keys
+  modifier = "Mod4";
+  altModifier = "Mod1";
+  smodifier = "Shift";
+  # Application
+  wmAppLauncher = "lxqt-runner";
+  wmAppTerminal = "alacritty";
+  wmAppBrowser = "google-chrome-stable";
+  wmClipboardManager = "clipmenu";
+}
