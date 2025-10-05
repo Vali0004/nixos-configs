8798cbf25699a9e50e5288667d816b493d2a3660
Vali0004 <diorcheats.vali@gmail.com>
2025-10-04 21:16:58 -0400

    Fix nixpkgs url

--- lenovo/boot/zfs-patch.nix
+++ lenovo/boot/zfs-patch.nix
@@ -5,7 +5,7 @@ let
     url = "https://github.com/openzfs/zfs/archive/pull/14013/head.tar.gz";
     hash = "sha256-X4PO6uf/ppEedR6ZAoWmrDRfHXxv2LuBThekRZOwmoA=";
   };
-  nixpkgsFlake = builtins.getFlake "github:NixOS/nixpkgs/nixos-unstable/59e69648d345d6e8fef86158c555730fa12af9de";
+  nixpkgsFlake = builtins.getFlake "github:NixOS/nixpkgs/59e69648d345d6e8fef86158c555730fa12af9de";
   nixpkgsPkgs = nixpkgsFlake.outputs.packages.x86_64-linux;
 in {
   nixpkgs.overlays = [
