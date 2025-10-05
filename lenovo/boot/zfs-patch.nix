9ef94445c7d475a43e401a18398c25c60622fbfb
Vali0004 <diorcheats.vali@gmail.com>
2025-10-04 21:30:21 -0400

    Revert changes

--- lenovo/boot/zfs-patch.nix
+++ lenovo/boot/zfs-patch.nix
@@ -5,21 +5,7 @@ let
     url = "https://github.com/openzfs/zfs/archive/pull/14013/head.tar.gz";
     hash = "sha256-X4PO6uf/ppEedR6ZAoWmrDRfHXxv2LuBThekRZOwmoA=";
   };
-  nixpkgs = builtins.getFlake "github:NixOS/nixpkgs/59e69648d345d6e8fef86158c555730fa12af9de";
-  pkgsOverride = import nixpkgs {
-    system = "x86_64-linux";
-    config = {
-      allowUnfree = true;
-      hostPlatform = "x86_64-linux";
-      permittedInsecurePackages = [
-        "libxml2-2.13.8"
-        "qtwebengine-5.15.19"
-      ];
-    };
-  };
 in {
-  nixpkgs.pkgs = pkgsOverride;
-
   nixpkgs.overlays = [
     (self: super: {
       zfs = super.zfs.overrideAttrs (old: {
