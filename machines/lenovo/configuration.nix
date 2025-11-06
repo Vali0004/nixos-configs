5487424ff3c42bf8e87f83b3b4223bd367b12b90
Vali0004 <diorcheats.vali@gmail.com>
2025-11-05 21:44:03 -0500

    Change lsusb rules, modify lenovo to add Flatpak and LACT

--- machines/lenovo/configuration.nix
+++ machines/lenovo/configuration.nix
@@ -75,9 +75,37 @@
   };
 
   networking = {
+    dhcpcd = {
+      # TP-Link is stupid...
+      #
+      # eth0: adding route to fdb5:8d30:9e81:1::/64 via fe80::1691:38ff:fed0:2729
+      # eth0: dhcp_envoption 24.0/3: malformed embedded option
+      # eth0: deleting route to fdb5:8d30:9e81:1::/64 via fe80::1691:38ff:fed0:2729
+      #
+      # Why is my router vomitting malformed DHCPv6 packets,
+      # and killing networking?
+      # Dumbest thing ever.
+      extraConfig = ''
+        nohook resolv.conf
+        # Stop dhcpcd from ever requesting vendor class or FQDN
+        nooption rapid_commit
+        nooption vendorclassid
+        nooption fqdn
+        nooption 24
+        nooption 25
+      '';
+      IPv6rs = true;
+    };
+    nameservers = [
+      "10.0.0.244"
+      "75.75.75.75"
+      "2601:406:8100:91d8:9504:1cf3:185b:1fa4"
+      "2001:558:FEED::1"
+    ];
     hostId = "2632ac4c";
     hostName = "lenovo";
-    useDHCP = true;
+    interfaces.wlan0.useDHCP = true;
+    useDHCP = false;
     usePredictableInterfaceNames = false;
   };
 
@@ -101,16 +129,47 @@
   qt.enable = true;
 
   security = {
-    rtkit.enable = true;
+    # Fucking realtime priority
+    rtkit.enable = lib.mkForce false;
     sudo.enable = true;
   };
 
   services = {
+    flatpak.enable = true;
+    # Linux GPU Configuration And Monitoring Tool
+    lact.enable = true;
     udev.extraRules = ''
-      # Keyboard
-      SUBSYSTEMS=="usb", ATTRS{idVendor}=="2e3c|8089", ATTRS{idProduct}=="c365|0009", GROUP="wheel"
+      # Aula, SayoDevice O3C
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="2e3c|8089", ATTRS{idProduct}=="c365|0009", GROUP="plugdev", MODE="0666"
+      # HTC
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="0bb4", GROUP="plugdev", MODE="0666"
+      # Oculus
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="2833", GROUP="plugdev", MODE="0666"
+      SUBSYSTEM=="hidraw", ATTRS{idVendor}=="2833", GROUP="plugdev", MODE="0666"
+      # SlimeVR
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="1209", GROUP="plugdev", MODE="0666"
+      # Sony - 054c:0fa8
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="054c", GROUP="plugdev", MODE="0666"
+      # Steam
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="28de", GROUP="plugdev", MODE="0666"
+      # Razer
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="1532", GROUP="plugdev", MODE="0666"
       # RedOctane
-      SUBSYSTEMS=="usb", ATTRS{idVendor}=="1430", GROUP="wheel"
+      SUBSYSTEM=="hidraw", ATTRS{idVendor}=="1430", TAG+="uaccess", MODE="0666", GROUP="plugdev"
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="1430", TAG+="uaccess", MODE="0666", GROUP="plugdev"
+      # Espressif
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="303a", GROUP="plugdev", MODE="0666"
+      # Set /dev/bus/usb/*/* as read-write for the plugdev group (0666) for Nordic Semiconductor devices
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="1915", MODE="0666"
+      # Set /dev/bus/usb/*/* as read-write for the plugdev group (0666) for WCH-CN devices
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="1a86", MODE="0666"
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="1d6b", MODE="0666"
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="1209", MODE="0666"
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="0bda", MODE="0666"
+      # USB CDC ACM for Nordic + Espressif
+      KERNEL=="ttyACM[0-9]*", SUBSYSTEM=="usb", ATTRS{idVendor}=="1915|303a", MODE="0666", ENV{CDC_ACM}="1"
+      KERNEL=="ttyACM[0-9]*", SUBSYSTEM=="tty", ATTRS{idVendor}=="1915|303a", MODE="0666", ENV{CDC_ACM}="1"
+      ENV{CDC_ACM}=="1", ENV{ID_MM_CANDIDATE}="0", ENV{ID_MM_DEVICE_IGNORE}="1"
     '';
     # upower daemon
     upower.enable = true;
@@ -137,11 +196,17 @@
       isNormalUser = true;
       extraGroups = [
         "corectrl"
+        "dialout"
         "input"
+        "openrazer"
+        "plugdev"
+        "qemu-libvirtd"
         "render"
         "tty"
-        "wheel"
         "video"
+        "wheel"
+        "wireshark"
+        "usbmon"
       ];
       openssh.authorizedKeys.keys = my_keys;
       shell = pkgs.zsh;
