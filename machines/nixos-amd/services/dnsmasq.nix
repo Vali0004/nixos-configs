11ca27e2307c416584df6fba44bd566de6fa1c63
Vali0004 <diorcheats.vali@gmail.com>
2025-11-23 08:01:20 -0500

    Setup IPv4/6 for eth0 NAT, fix dnsmasq

--- machines/nixos-amd/services/dnsmasq.nix
+++ machines/nixos-amd/services/dnsmasq.nix
@@ -14,7 +14,7 @@
       dhcp-range = [
         "192.168.0.2,192.168.0.254"
         "2001:db8:1::1000,2001:db8:1::2000,64,12h"
-        "::,constructor:eth0,64,ra-stateless,12h"
+        "::,constructor:eth0,ra-stateless,12h"
       ];
       enable-ra = true;
       interface = [
@@ -27,8 +27,18 @@
     };
   };
 
-  networking.firewall = {
-    extraCommands = ''
+  networking = {
+    interfaces.eth0 = {
+      ipv4.addresses = [{
+        address = "192.168.0.1";
+        prefixLength = 24;
+      }];
+      ipv6.addresses = [{
+        address = "2001:db8:1::1";
+        prefixLength = 64;
+      }];
+    };
+    firewall.extraCommands = ''
       ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE
     '';
   };
