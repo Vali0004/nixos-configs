fdcd921c7a381bd189f357fa85d80548d60b4775
Vali0004 <diorcheats.vali@gmail.com>
2025-11-29 23:47:04 -0500

    Fix scripts.nix

--- overlays/module.nix
+++ overlays/module.nix
@@ -12,6 +12,14 @@
   nixpkgs.overlays = [
     (import ./customPackages.nix)
     (import ./existingPackages.nix)
-    (import ./scripts.nix { inherit config; })
+    (self: super: {
+      fastfetch-simple = self.writeScriptBin "fastfetch-simple" ''
+        ${self.fastfetch}/bin/fastfetch --config /home/vali/.config/fastfetch/simple.jsonc
+      '';
+      flameshot-upload = self.writeScriptBin "flameshot-upload" ''
+        ${self.flameshot}/bin/flameshot gui --accept-on-select -r > /tmp/screenshot.png
+        ${self.curl}/bin/curl -H "$(${self.coreutils}/bin/cat /run/agenix/zipline-upload-headers)" https://holy.fuckk.lol/api/upload -F file=@/tmp/screenshot.png -H 'content-type: multipart/form-data' | ${self.jq}/bin/jq -r .files[0url | tr -d '\n' | ${self.xclip}/bin/xclip -selection clipboard
+      '';
+    })
   ];
 }
