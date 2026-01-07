6fce24edce6421235abe175eddf09b6691c0526d
Vali0004 <diorcheats.vali@gmail.com>
2026-01-06 21:59:57 -0500

    Fix hosts file

--- modules/networking/hosts.nix
+++ modules/networking/hosts.nix
@@ -1,12 +1,12 @@
 {
   networking.extraHosts = ''
     10.0.0.1 nixos-router
-    10.0.0.4 shitzen-nixos
-    10.0.0.7 kvm-shitzen
     10.0.0.2 nixos-shitclient
     10.0.0.3 nixos-hass
-    10.0.0.189 nixos-amd
+    10.0.0.4 shitzen-nixos
+    10.0.0.5 kvm-shitzen
     10.0.0.31 lenovo
+    10.0.0.189 nixos-amd
     10.0.0.124 chromeshit
     142.167.45.55 cleverca22
     74.208.44.130 router-vps
