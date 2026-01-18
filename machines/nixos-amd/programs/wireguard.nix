42f6a09e8754e886b7ff1d1e8d36b534301f965b
Vali0004 <diorcheats.vali@gmail.com>
2026-01-17 20:44:32 -0500

    Fix wireguard config

--- machines/nixos-amd/programs/wireguard.nix
+++ machines/nixos-amd/programs/wireguard.nix
@@ -2,9 +2,38 @@
 , ... }:
 
 {
+  environment.systemPackages = [ pkgs.wireguard-tools ];
+
+  networking = {
+    networkmanager.unmanaged = [
+      "interface-name:wg0"
+    ];
+    firewall = {
+      allowPing = true;
+      allowedUDPPorts = [ 51820 ];
+      checkReversePath = false;
+      interfaces.wg0 = {
+        allowedTCPPortRanges = [{
+          from = 0;
+          to = 65535;
+        }];
+        allowedUDPPortRanges = [{
+          from = 0;
+          to = 65535;
+        }];
+      };
+    };
+    nat = {
+      enable = false;
+      externalInterface = "enp10s0";
+      internalInterfaces = [ "wg0" ];
+    };
+    interfaces.wg0.useDHCP = false;
+  };
+
   networking.wg-quick.interfaces.wg0 = {
     address = [ "10.5.5.2/32" ];
-    privateKeyFile = config.age.secrets.wireguard-private-yuts.path;
+    privateKeyFile = config.age.secrets.wireguard-private-yutsu.path;
     peers = [{
       allowedIPs = [ "10.5.5.0/24" "10.0.0.0/24" ];
       endpoint = "24.65.132.48:51820";
